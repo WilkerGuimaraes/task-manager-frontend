271c1feaf6c18b5a71d33feb4f4e14fca397c7b7
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-10 22:08:46 -0300

    feat: add Home page

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,23 @@
+import { Tasks } from "../components/tasks_/tasks";
+
+import { ToastContainer } from "react-toastify";
+
+export function Home() {
+  return (
+    <div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <Tasks />
+    </div>
+  );
+}
